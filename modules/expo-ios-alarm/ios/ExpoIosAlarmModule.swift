0e98cea819e905e9d66e4fbb69b822dfc0100c90
cirriole <cirriole@gmail.com>
2026-05-26 05:29:57 +0900

    fix: Restore alertPresentation as it is required by AlarmKit

--- modules/expo-ios-alarm/ios/ExpoIosAlarmModule.swift
+++ modules/expo-ios-alarm/ios/ExpoIosAlarmModule.swift
@@ -67,8 +67,27 @@ public class ExpoIosAlarmModule: Module {
             let customMetadata = AlarmData(alarmID: id.uuidString, stationName: stationName)
             let secondaryIntent = StopIntent(alarmID: id.uuidString)
             
+            let stopButton = AlarmButton(
+                text: "スライドで閉じる",
+                textColor: .white,
+                systemImageName: "xmark.circle"
+            )
+            
+            let customStopButton = AlarmButton(
+                text: "停止する",
+                textColor: .white,
+                systemImageName: "stop.circle.fill"
+            )
+            
+            let alertPresentation = AlarmPresentation.Alert(
+                title: "🔔 まもなく \(stationName) です！",
+                stopButton: stopButton,
+                secondaryButton: customStopButton,
+                secondaryButtonBehavior: .custom
+            )
+            
             let attributes = AlarmAttributes<AlarmData>(
-                presentation: AlarmPresentation(),
+                presentation: AlarmPresentation(alert: alertPresentation),
                 metadata: customMetadata,
                 tintColor: Color.orange
             )
